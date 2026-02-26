a0315d6a30300abfdf6f66e907fd75896a7603bc
mirkoferretti1 <mirko.ferretti@outlook.com>
2026-02-26 15:47:36 +0100

    Create R_script.R

---
+++ Code/R_script.R
@@ -0,0 +1,33 @@
+#First R script
+
+#oggetti e assegnazione
+
+michele<- 2+3
+
+tecla<-4+6
+
+michele + tecla
+
+michele/tecla
+
+#vettori (arrays)
+
+sonia <- c(10, 8, 3, 1, 0)  #funzione e argomenti
+
+giorgia <- c(3, 10, 20, 50, 100)
+
+plot(giorgia, sonia)
+
+plot(giorgia, sonia, xlab="Conc. inquinanti", ylab="N. delfini", col="red", pch=19, cex=2)
+
+#installazione pacchetti
+#CRAN
+install.packages("terra")
+
+library(terra)
+
+#GitHub 
+install.packages("devtools")
+library(devtools)
+
+install_github("ducciorocchini/imageRy")
