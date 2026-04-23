e72e8bee60cb2b17eca5dd688c83f5748b2d57ee
mirkoferretti1 <mirko.ferretti@outlook.com>
2026-04-23 15:43:52 +0200

    Create 08_functions.R

---
+++ Code/08_functions.R
@@ -0,0 +1,18 @@
+# La mia funzione!!! XD
+
+somma <- function(x,y){
+  z=x+y
+  return(z)
+  }
+
+somma(1, 100)
+
+#esercizio: fare una funzione chiamata differenza
+
+differenza <- function(x,y){
+  z=x-y
+  return(z)
+  }
+
+differenza(1, 100)
+
